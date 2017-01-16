ba8dcc4d2b643ed576e27b4641d44f952eff5bda
Björn Kjeholt <dev@kjeholt.se>
2017-01-16 21:38:20 +0100

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -14,13 +14,12 @@ BUILD_ARCHITECTURE=rpi
 DOCKER_IMAGE_NAME=${1}
 DOCKER_IMAGE_BASE_TAG=${GITHUB_BRANCH}-${BUILD_NO}
 DOCKER_IMAGE_FULL_NAME=${DOCKER_HUB_NAME}/${DOCKER_IMAGE_NAME}
-DOCKER_IMAGE_FULL_TAG=${GITHUB_BRANCH}-${BUILD_NO}-${BUILD_ARCHITECTURE}
-DOCKER_IMAGE_REV_TAG=${GITHUB_BRANCH}-${BUILD_ARCHITECTURE}
-DOCKER_IMAGE_LATEST_TAG=latest-${BUILD_ARCHITECTURE}
+DOCKER_IMAGE_TAG=${GITHUB_BRANCH}-${BUILD_NO}-${BUILD_ARCHITECTURE}
+DOCKER_IMAGE_CLEAN_TAG=${GITHUB_BRANCH}-${BUILD_ARCHITECTURE}
 
-DEVELOPMENT_DIRECTORY=${DOCKER_IMAGE_NAME}-${GITHUB_BRANCH}
+DEVELOPMENT_DIRECTORY=${DOCKER_IMAGE_NAME}-${DOCKER_IMAGE_BASE_TAG}
 
-yes | rm -r ${DEVELOPMENT_DIRECTORY}
+yes | rm -r ${DOCKER_IMAGE_NAME}-${GITHUB_BRANCH}*
 
 echo "-------------------------------------------------------------------------------"
 echo "Clone project from Github "
@@ -39,24 +38,19 @@ echo "Build the project "
 echo " Directory:       ${DEVELOPMENT_DIRECTORY} "
 echo "-------------------------------------------------------------------------------"
 
-DOCKERFILE_PATH=${DEVELOPMENT_DIRECTORY}/Dockerfile-${BUILD_ARCHITECTURE}
+cd ${DEVELOPMENT_DIRECTORY}
+
+DOCKERFILE_PATH=Dockerfile-${BUILD_ARCHITECTURE}
 
 # Start the build
 
 docker build -f ${DOCKERFILE_PATH} \
-             --build-arg APPLICATION_REVISION=${GITHUB_BRANCH}-${BUILD_NO} \
-             -t ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_FULL_TAG} \
-             -t ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_REV_TAG} \
-             -t ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_LATEST_TAG} .
+             --build-arg DOCKER_IMAGE_NAME=${DOCKER_IMAGE_FULL_NAME} \
+             --build-arg DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG} \
+             -t ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_TAG} \
+             -t ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_CLEAN_TAG} \
+             .
 
-echo "-------------------------------------------------------------------------------"
-echo "Export to docker hub "
-echo " Repository:     ${DOCKER_IMAGE_FULL_NAME} "
-echo " Tag:            ${DOCKER_IMAGE_LATEST_TAG} "
-echo "                 ${DOCKER_IMAGE_REV_TAG} "
-echo "                 ${DOCKER_IMAGE_FULL_TAG} "
-echo "-------------------------------------------------------------------------------"
+docker push ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_TAG}
+docker push ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_CLEAN_TAG}
 
-docker push ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_FULL_TAG}
-docker push ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_REV_TAG}
-docker push ${DOCKER_IMAGE_FULL_NAME}:${DOCKER_IMAGE_LATEST_TAG}
